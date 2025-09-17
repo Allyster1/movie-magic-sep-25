a11490d40a5150e7e7a86152fda7919079f231ca
Slavi Ovardov <ovardovslav@gmail.com>
2025-09-17 21:38:13 +0300

    Modify search form

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -24,6 +24,9 @@ movieController.get("/:movieId/details", (req, res) => {
 
 movieController.get("/search", (req, res) => {
    const movies = movieService.getAll();
+
+   console.log(req.query);
+
    res.render("search", { movies });
 });
 
