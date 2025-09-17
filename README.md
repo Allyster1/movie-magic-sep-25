59a2aa8b66e45a39e1f1acba9ab40fd082ed9d2b
Slavi Ovardov <ovardovslav@gmail.com>
2025-09-17 18:26:28 +0300

    add express handlebars setup

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ SoftUni JS Back End Course Workshop
 - [x] Initialize Project
 - [x] Add Express Server
 - [x] Add Workshop Resources
-- [] Setup Handlebars
+- [x] Setup Handlebars
 - [] Render home page
--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import express from "express";
+import handlebars from "express-handlebars";
 
 const app = express();
 
+// Setup Handlebars
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: "hbs",
+  })
+);
+
+app.set("view engine", "hbs");
+app.set("views", "src/views");
+
+// Routes
 app.get("/", (req, res) => {
-  res.send("it works");
+  res.render("home", { layout: false });
 });
 
+// Start Server
 app.listen(5000, () =>
   console.log("Server is running on port http://localhost:5000")
 );
